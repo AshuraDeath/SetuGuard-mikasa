812c3f0eb628ea605ab4e5af8aac7f397d1fd5a5
ranjit95Ashura <ranjitsuryawanshi952@gmail.com>
2025-06-01 01:32:28 +0530

    module imported

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,25 +1,34 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from './common/common.module';
-import { DomainCheckModule } from './modules/domain-check/domain-check.module';
 import { AuthModule } from './modules/auth/auth.module';
-
+import { CertGuardModule } from './modules/cert-guard/cert-guard.module';
+import { DnsProtectModule } from './modules/dns-protect/dns-protect.module';
+import { DomainCheckModule } from './modules/domain-check/domain-check.module';
+import { FeedbackModule } from './modules/feedback/feedback.module';
+import { HealthModule } from './modules/health/health.module';
+import { RPKIVerifyModule } from './modules/rpki-verify/rpki-verify.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1010',
-      database: 'setuguard',
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: process.env.NODE_ENV === 'development',
-      logging: process.env.NODE_ENV === 'development',
+      synchronize: process.env.NODE_ENV === process.env.NODE_ENV,
+      logging: process.env.NODE_ENV === process.env.NODE_ENV,
     }),
     CommonModule,
-    DomainCheckModule,
     AuthModule,
+    CertGuardModule,
+    DnsProtectModule,
+    DomainCheckModule,
+    FeedbackModule,
+    HealthModule,
+    RPKIVerifyModule,
   ],
   providers: [],
   exports: [],
